528cd0ea188ee22c2f3f256ff5769f6d019705ec
mstava <mustafaabdo468@gmail.com>
2020-11-13 19:25:41 +0200

    init the project with expo

---
+++ README.md
@@ -0,0 +1,13 @@
+####Sovir is an e-commerce application for shopping clothes
+
+##### requirements
+
+- React Native
+- TypeScript
+- Expo
+
+##### installing and running
+
+- `yarn install`
+- `yarn start`
+
---
+++ types.tsx
@@ -0,0 +1,32 @@
+export type RootStackParamList = {
+  Root: undefined;
+  NotFound: undefined;
+};
+
+export type BottomTabParamList = {
+  Home: undefined;
+  Category: undefined;
+  Cart: undefined;
+  Favorite: undefined;
+  Profile: undefined;
+};
+
+export type HomeParamList = { HomeScreen: undefined; };
+export type CategoryParamList = { CategoryScreen: undefined; };
+export type CartParamList = { CartScreen: undefined; };
+export type FavoriteParamList = { FavoriteScreen: undefined; };
+export type ProfileParamList = { ProfileScreen: undefined; };
+
+export type Product = {
+  id: string,
+  name: string,
+  brand: string,
+  image: string,
+  price: string,
+  rating: string
+}
+
+export type ProductsList = {
+  label: string,
+  description: string,
+}
