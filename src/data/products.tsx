0f92fffe1394f2a255253c00a4f7b67eb5d7c2be
mstava <mustafaabdo468@gmail.com>
2020-11-25 17:47:08 +0200

    add description and product images data

--- src/data/products.tsx
+++ src/data/products.tsx
@@ -9,7 +9,25 @@ export default [
         id: '1',
         name: 'Blouse',
         brand: 'Dorothy Perkins',
-        image: Blouse,
+        description: 'Short dress in soft cotton jersey with decorative buttons down the front and a wide, frill-trimmed square neckline with concealed elastication. Elasticated seam under the bust and short puff sleeves with a small frill trim.',
+        cover_image: Blouse,
+        product_images: [
+            {
+                id: '1',
+                image_name: Blouse,
+                path: ''
+            },
+            {
+                id: '2',
+                image_name: Blouse,
+                path: ''
+            },
+            {
+                id: '3',
+                image_name: Blouse,
+                path: ''
+            },
+        ] ,
         price: '12',
         rating: '10'
     },
@@ -17,7 +35,25 @@ export default [
         id: '2',
         name: 'Evening Dress',
         brand: 'Silly',
-        image: EveningDress,
+        description: 'Short dress in soft cotton jersey with decorative buttons down the front and a wide, frill-trimmed square neckline with concealed elastication. Elasticated seam under the bust and short puff sleeves with a small frill trim.',
+        cover_image: EveningDress,
+        product_images: [
+            {
+                id: '1',
+                image_name: EveningDress,
+                path: ''
+            },
+            {
+                id: '2',
+                image_name: EveningDress,
+                path: ''
+            },
+            {
+                id: '3',
+                image_name: EveningDress,
+                path: ''
+            },
+        ] ,
         price: '19',
         rating: '55'
     },
@@ -25,7 +61,25 @@ export default [
         id: '3',
         name: 'White Sports Dress',
         brand: 'OVS',
-        image: WhiteSportsDress,
+        description: 'Short dress in soft cotton jersey with decorative buttons down the front and a wide, frill-trimmed square neckline with concealed elastication. Elasticated seam under the bust and short puff sleeves with a small frill trim.',
+        cover_image: WhiteSportsDress,
+        product_images: [
+            {
+                id: '1',
+                image_name: WhiteSportsDress,
+                path: ''
+            },
+            {
+                id: '2',
+                image_name: WhiteSportsDress,
+                path: ''
+            },
+            {
+                id: '3',
+                image_name: WhiteSportsDress,
+                path: ''
+            },
+        ] ,
         price: '33',
         rating: '13'
     },
@@ -33,7 +87,25 @@ export default [
         id: '4',
         name: 'T-Shirt Sailing',
         brand: 'Mango Boy',
-        image: Shirt,
+        description: 'Short dress in soft cotton jersey with decorative buttons down the front and a wide, frill-trimmed square neckline with concealed elastication. Elasticated seam under the bust and short puff sleeves with a small frill trim.',
+        cover_image: Shirt,
+        product_images: [
+            {
+                id: '1',
+                image_name: Shirt,
+                path: ''
+            },
+            {
+                id: '2',
+                image_name: Shirt,
+                path: ''
+            },
+            {
+                id: '3',
+                image_name: Shirt,
+                path: ''
+            },
+        ] ,
         price: '150',
         rating: '5'
     },
@@ -41,7 +113,25 @@ export default [
         id: '5',
         name: 'Black Sports Dress',
         brand: 'Cool',
-        image: BlackSportsDress,
+        description: 'Short dress in soft cotton jersey with decorative buttons down the front and a wide, frill-trimmed square neckline with concealed elastication. Elasticated seam under the bust and short puff sleeves with a small frill trim.',
+        cover_image: BlackSportsDress,
+        product_images: [
+            {
+                id: '1',
+                image_name: BlackSportsDress,
+                path: ''
+            },
+            {
+                id: '2',
+                image_name: BlackSportsDress,
+                path: ''
+            },
+            {
+                id: '3',
+                image_name: BlackSportsDress,
+                path: ''
+            },
+        ] ,
         price: '25',
         rating: '40'
     },
