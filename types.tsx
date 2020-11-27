b530b56923d13c79f43c6f3751c4fc5021975dad
mstava <mustafaabdo468@gmail.com>
2020-11-27 22:50:43 +0200

    add screens types, profile categories and order types

--- types.tsx
+++ types.tsx
@@ -1,10 +1,32 @@
+import ShippingScreen from "./src/screens/ProfileStack/SubScreens/ShippingScreen";
+import PaymentScreen from "./src/screens/ProfileStack/SubScreens/PaymentScreen";
+import PromoScreen from "./src/screens/ProfileStack/SubScreens/PromoScreen";
+import ReviewScreen from "./src/screens/ProfileStack/SubScreens/ReviewScreen";
+import SettingScreen from "./src/screens/ProfileStack/SubScreens/SettingScreen";
+import * as React from "react";
+import OrderDetailScreen from "./src/screens/ProfileStack/SubScreens/OrderDetailScreen";
+import RegisterScreen from "./src/screens/AuthStack/SubScreens/RegisterScreen";
+import CheckoutScreen from "./src/screens/CartStack/SubScreens/CheckoutScreen";
+
 export type RootStackParamList = {
   Root: undefined;
+  Main: undefined;
   NotFound: undefined;
   NewScreen: undefined;
   SaleScreen: undefined;
   DetailScreen: undefined;
   MyOrderScreen: undefined;
+  ShippingScreen: undefined;
+  PaymentScreen: undefined;
+  PromoScreen: undefined;
+  ReviewScreen: undefined;
+  SettingScreen: undefined;
+  OrderDetailScreen: undefined;
+  RegisterScreen: undefined;
+  LoginScreen: undefined;
+  ForgotPasswordScreen: undefined;
+  CheckoutScreen: undefined;
+  SuccessScreen: undefined;
 };
 
 export type BottomTabParamList = {
@@ -43,7 +65,7 @@ export type ProductsList = {
   description: string,
 }
 
-export type HeaderPage = {
+export type Title = {
   title: string
 }
 
@@ -51,4 +73,20 @@ export type CategoryList = {
   id: string,
   name: string,
   image: string
+}
+
+export type ProfileCategories = {
+  id: string,
+  name: string,
+  info: string
+}
+
+export type Order = {
+  id: string,
+  order_number: string,
+  tracking_number: string,
+  quantity: string,
+  total_amount: string,
+  date: string,
+  status: string
 }
