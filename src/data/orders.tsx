1282f44f7ac2d95ccee08770cf287ab71a32d339
mstava <mustafaabdo468@gmail.com>
2020-11-27 22:51:49 +0200

    add  order data

---
+++ src/data/orders.tsx
@@ -0,0 +1,29 @@
+export default [
+    {
+        id: '1',
+        order_number: '1947034',
+        tracking_number: 'IW3475453455',
+        quantity: '3',
+        total_amount: '112',
+        date: '05-12-2019',
+        status: 'Delivered'
+    },
+    {
+        id: '2',
+        order_number: '1947034',
+        tracking_number: 'IW3475453455',
+        quantity: '3',
+        total_amount: '112',
+        date: '05-12-2019',
+        status: 'Delivered'
+    },
+    {
+        id: '3',
+        order_number: '1947034',
+        tracking_number: 'IW3475453455',
+        quantity: '3',
+        total_amount: '112',
+        date: '05-12-2019',
+        status: 'Delivered'
+    },
+]
